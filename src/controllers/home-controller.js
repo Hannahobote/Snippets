ce730e81c390d654a3bd33b0db3543d14d907f50
Hannah Obote <ao223ir@student.lnu.se>
2021-03-09 14:23:08 +0100

    working on creating new snippet

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,13 +1,38 @@
 import moment from 'moment'
 import { Snippets } from '../models/snippets.js'
+const data = [
+  {
+    title: 'php',
+    user: 'member1',
+    id: 1,
+    description: 'hello world, im member 1 ',
+    snippet: `
+    temp = $1
+    $1 = $2
+    $2 = temp
+    `,
+    done: false
+  },
+  {
+    title: ' java ',
+    user: 'member2',
+    id: 2,
+    description: 'im ´member 2',
+    snippet: `
+    temp = x
+    x = y
+    y = temp
+    `,
+    done: false
+  }
+]
+
 /**
  *Hi.
  */
 export class HomeController {
   /**
-   * Renders a view and sends the rendered HTML sting as an http resone.
-   * index GET.
-   * READ.
+   *Displays a list of Snippets.
    *
    * @param {object} req - Express request obj.
    * @param {object} res - Express response obj.
@@ -15,17 +40,62 @@ export class HomeController {
    */
   async index (req, res, next) {
     try {
-     const viewData = {
-        snippets: (await Snippets.find({}))
+      const viewData = {
+        snippets: await Snippets.find({})
           .map(snippets => ({
             id: snippets._id,
-            description: snippets.description,
-            done: snippets.done
+            createdAt: moment(snippets.createdAt).fromNow(),
+            value: snippets.value
           }))
       }
-     res.render('home/index', { viewData })
+      // res.render('home/index', { dummyData })
+      res.render('home/index', { data })
+      console.log('from index function:', viewData, await Snippets())
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  /**
+   * Returns a HTML form for creating a snippet.
+   *
+   * @param {object} req - Express request obj.
+   * @param {object} res - Express response obj.
+   */
+  async new (req, res) {
+    try {
+      res.render('home/new')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /**
+   * Creates a snippet.
+   *
+   * @param {object} req - Express request obj.
+   * @param {object} res - Express response obj.
+   * @param {Function} next - Express next middlewere function.
+   */
+  async create (req, res, next) {
+    try {
+      // create a snippet
+      const snippet = new Snippets({
+        value: req.body.value
+      })
+
+      // save to database
+      await snippet.save()
+
+      // ...and redirect and show a message.
+      req.session.flash = { type: 'success', text: 'The pure number was saved successfully.' }
+      res.redirect('.')
+      res.render('home/index', { snippet })
+    } catch (error) {
+      res.render('home/new', {
+        validationErrors: [error.message] || [error.errors.value.message],
+        value: req.body.value
+      })
     }
   }
 
@@ -51,51 +121,17 @@ export class HomeController {
    * @param {object} res - Express response obj.
    * @param {Function} next - Express next middlewere function.
    */
-  async create (req, res, next) {
-    try {
-      const snippet = new Snippets({
-        description: req.body.description,
-        done: req.body.done
-      })
-      await snippet.save()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  /**
-   * Renders a view, based on posted data, and send the rendered HTML as sting as an http response.
-   *
-   * @param {object} req - Express request obj.
-   * @param {object} res - Express response obj.
-   * @param {Function} next - Express next middlewere function.
-   */
-  async new (req, res, next) {
-    try {
-      const snippet = new Snippets({
-        description: req.body.description,
-        done: req.body.done
-      })
-      await snippet.save()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  /**
-   * Renders a view, based on posted data, and send the rendered HTML as sting as an http response.
-   *
-   * @param {object} req - Express request obj.
-   * @param {object} res - Express response obj.
-   * @param {Function} next - Express next middlewere function.
-   */
   async edit (req, res, next) {
     try {
-      const snippet = new Snippets({
+      const snippet = await Snippets.findById(req.params.id)
+      const { _id: id, title, description } = snippet
+      const data = { id, title, description }
+      return data
+      /* const snippet = new Snippets({
         description: req.body.description,
         done: req.body.done
       })
-      await snippet.save()
+      await snippet.save() */
     } catch (error) {
       console.log(error)
     }
--- src/models/snippets.js
+++ src/models/snippets.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
 
 export const SnippetsSchema = new mongoose.Schema({
   description: {
+    title: String,
+    id: uuidv4(),
     type: String,
     required: true,
     trim: true,
