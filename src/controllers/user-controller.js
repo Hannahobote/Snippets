ebf94c11f7b2021e5ff7708fe22113e185666a16
Hannah Obote <ao223ir@student.lnu.se>
2021-03-23 20:44:31 +0100

    create account ad login succssesful ? fix bug

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -14,14 +14,17 @@ export class UserController {
    */
   async createAccount (req, res, next) {
     try {
-      // Create a new user.
-      const user = new User(req.body)
       // Hash and salt the password.
-      // const password = await bcrypt.hash(req.body.passowrd, 8)
-
-      // save user to database.
-      console.log(user)
-      user.save()
+      await bcrypt.hash(req.body.password, 10).then(function (hash) {
+        // Create a new user.
+        const user = new User({
+          username: req.body.username,
+          password: hash
+        })
+        // save user to database.
+        user.save()
+        console.log('from create function', user)
+      })
       res.render('snippets/create-account')
     } catch (error) {
       console.log(error)
@@ -38,8 +41,8 @@ export class UserController {
   async login (req, res, next) {
     try {
       // find user in database
-       const user = await User.findOne({ username: req.body.username })
-      //const hashedPassword = await bcrypt.hash(req.body.password, 10
+      const user = await User.findOne({ username: req.body.username })
+      // const hashedPassword = await bcrypt.hash(req.body.password, 10
       if (user) {
         // const validPassword = await bcrypt.compare(req.body.password, user.password)
         console.log('password is correct')
@@ -54,21 +57,38 @@ export class UserController {
   }
 
   /**
+   *Pre logout.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware.
+   */
+  preLogout (req, res, next) {
+    res.render('snippets/pre-logout')
+  }
+
+  /**
    *Logout.
    *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware.
    */
   logout (req, res, next) {
     try {
+      console.log(req.session.username)
       delete req.session.user
       if (!req.session.user) {
         req.session.flash = { type: 'success', text: 'Logout successful.' }
+        console.log('user logged out', req.session.username)
         res.redirect('..')
       }
     } catch (error) {
       next(error)
     }
   }
-    /**
+
+  /**
    * Redirect to login.
    *
    * @param {object} req - Express request object.
@@ -76,7 +96,7 @@ export class UserController {
    * @param {Function} next - Express next middleware function.
    */
   async test (req, res, next) {
-    const user = {user: req.body.username , password: req.body.password}
+    const user = { user: req.body.username, password: req.body.password }
     console.log(user, req.body)
     res.render('snippets/test')
   }
--- src/routes/home-router.js
+++ src/routes/home-router.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { User } from '../models/user.js'
+import bcrypt from 'bcrypt'
 import { HomeController } from '../controllers/home-controller.js'
 import { UserController } from '../controllers/user-controller.js'
 
@@ -10,10 +12,43 @@ const user = new UserController()
 router.get('/', (req, res, next) => controller.index(req, res, next))
 router.post('/', (req, res, next) => controller.indexPost(req, res, next))
 
-// router.post('/login', (req, res, next) => controller.login(req, res, next))
-// router.post('/create-account', (req, res, next) => controller.createAccount(req, res, next))
+// router.get('/login', (req, res, next) => user.login(req, res, next))
+// router.get('/create-account', (req, res, next) => user.createAccount(req, res, next))
+router.get('/create-account-get', async (req, res, next) => {
+ res.render('snippets/create-account')
+})
 
-router.get('/login', (req, res, next) => user.login(req, res, next))
-router.get('/create-account', (req, res, next) => user.createAccount(req, res, next))
+router.get('/login-get', async (req, res, next) => {
+  res.render('snippets/login')
+ })
+
+router.post('/create-account', async (req, res, next) => {
+  const user = new User(req.body)
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
+  console.log(user)
+  user.save()
+})
+
+router.post('/login', async (req, res) => {
+  const body = req.body
+  const user = await User.findOne({ username: body.username})
+  if (user) {
+    // check user password with hashed password stored in the database
+    const validPassword = await bcrypt.compare(body.password, user.password)
+    if (validPassword) {
+      res.render('snippets/test')
+      // res.status(200).json({ message: 'Valid password' })
+    } else {
+      res.status(400).json({ error: 'Invalid Password' })
+    }
+  } else {
+    res.status(401).json({ error: 'User does not exist' })
+  }
+})
 
 router.post('/test', (req, res, next) => user.test(req, res, next))
+
+router.get('/pre-logout', (req, res, next) => user.preLogout(req, res, next))
+
+router.post('/logout', (req, res, next) => user.logout(req, res, next))
